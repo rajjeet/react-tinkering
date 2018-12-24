5276a2710eb2157c9e022258fdc2defedde15cbf
rajjeet <rajjeet.phull@gmail.com>
2018-12-24 13:41:52 -0500

    Added test for reducer

---
+++ src/reducers/__tests__/sampleReducer.spec.js
@@ -0,0 +1,21 @@
+import sampleReducer, {initialState} from '../sampleReducer';
+import actionTypes from "../../actions/actionTypes";
+
+describe("sampleReducer", () => {
+    it('doesnt modify state if no action is null', () => {
+        const action = {type: ""};
+        expect(sampleReducer(initialState, action)).toEqual(initialState);
+    });
+
+    it('doesnt modify state if action is undefined', () => {
+        const action = {type: undefined};
+        expect(sampleReducer(initialState, action)).toEqual(initialState);
+    });
+
+    it('increments the metric by payload when action type is SAMPLE_ACTION_START', () => {
+        const action = {type: actionTypes.SAMPLE_ACTION_START, payload: 1};
+        const expectedState = {...initialState, metric: initialState.metric + action.payload};
+        expect(sampleReducer(initialState, action)).toEqual(expectedState);
+    });
+
+});
--- src/reducers/sampleReducer.js
+++ src/reducers/sampleReducer.js
@@ -1,6 +1,6 @@
 import actionTypes from "../actions/actionTypes";
 
-const initialState = {
+export const initialState = {
     metric: 0
 };
 
