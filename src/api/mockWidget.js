a374f2779f9ffbb2d99b3206e53fbe1b6acdde5c
rajjeet <rajjeet.phull@gmail.com>
2018-12-27 08:19:46 -0500

    Added mock widget api

---
+++ src/api/mockWidget.js
@@ -0,0 +1,63 @@
+const widgets = [
+    {
+        id: '1',
+        name: 'Widget 1',
+        description: 'This widget is for display'
+    },
+    {
+        id: '2',
+        name: 'Widget 2',
+        description: 'This widget is for play'
+    },
+    {
+        id: '3',
+        name: 'Widget 3',
+        description: 'This widget is for storage'
+    }
+];
+
+
+const generateId = () => Math.max.apply(Math, widgets.map(o => o.id)) + 1;
+
+class WidgetApi {
+    static getAllWidgets() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(Object.assign([], widgets));
+            }, 200);
+        });
+    }
+
+    static saveWidget(widget){
+        widget = Object.assign({}, widget);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (widget.name.length < 3){
+                    reject(`Name of widget must be greater than 3 characters`);
+                }
+
+                if (widget.id){
+                    const index = widgets.findIndex(w => w.id === widget.id);
+                    widgets.splice(index, 1, widget);
+                } else {
+                    widget.id = generateId();
+                    widgets.push(widget);
+                }
+
+                resolve(widget);
+            }, 200);
+        });
+    }
+
+    static deleteAuthor(widgetId) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const index = widgets.findIndex(w => w.id === widgetId);
+                widgets.splice(index, 1);
+                resolve();
+            }, 200);
+        });
+    }
+}
+
+export default WidgetApi;
